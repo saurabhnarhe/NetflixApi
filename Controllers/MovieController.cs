1636f733c1a712d402ff57263a739f2796351a45
MayankGandhe <mayank.gandhe@gmail.com>
2021-10-06 11:29:00 +0530

    Update MovieController.cs

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
@@ -7,29 +7,21 @@ namespace APIPractical.Controllers
     [Route("movies")]
     public class MovieController : ControllerBase
     {
-        private static List<Movie> _movies = new List<Movie>()
-        {
-            new Movie()
-            {
-                Name = "ddlj",
-                Genre = "drama",
-                Year = 2015
-            },
-            new Movie()
-            {
-                Name = "matrix",
-                Genre = "si-fi",
-                Year = 1998
-            }
-        };
+        public static List<Movie> _movies ;// new Database().getData();
+        public void CacheData()
+        { 
+            if (_movies == null)
+                _movies = new Database().getData();
 
-
+        }
         [Route("")]
         [HttpGet]
         public List<Movie> GetAll()
         {
+            CacheData();
             return _movies;
         }
+
 
         [Route("{movieName}")]
         [HttpGet]
@@ -37,5 +29,59 @@ namespace APIPractical.Controllers
         {
             return _movies.Find(movie => movie.Name == movieName);
         }
+
+        [Route("revalidate")]
+        [HttpGet]
+        public List<Movie> RevalidateCache()
+        {
+            _movies = new Database().getData();
+
+            return _movies;
+        }
+
+        [Route("{movieName}")]
+        [HttpPost]
+        public List<Movie> PutMovie(string movieName)
+        {
+            var movie = new Movie()
+            {
+                Name = movieName,
+                Genre = "dr",
+                Year = 1999
+            };
+            _movies.Add(movie);
+            return _movies;
+        }
+
+        [Route("add")]
+        [HttpPost]
+        public List<Movie> AddMovie([FromBody]Movie movie)
+        {
+            _movies.Add(movie);
+            return _movies;
+
+        }
+
+
+        [Route("{movieName}")]
+        [HttpDelete]
+        public List<Movie> RemoveMovie(string movieName)
+        {
+            var movieToDelete = _movies.Find(movie => movie.Name == movieName);
+            _movies.Remove(movieToDelete);
+            return _movies;
+
+        }
+
+        [Route("year/{movieName}/{updateYear}")]
+        [HttpPut]
+        public Movie UpdateMovieYear(string movieName, int updateYear)
+        {
+            var index = _movies.IndexOf(_movies.Find(movie => movie.Name == movieName));
+            _movies[index].Year = updateYear;
+            return _movies[index];
+        }
+
+
     }
 }
